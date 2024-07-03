59c296a7a1b5ba4d7d4615d0adcc53a5bb07cff3
Mateusinbessa <mateusbessacontato@gmail.com>
2024-07-03 13:18:26 -0300

    Basic Types

---
+++ TypeScript/02 - Basic Types/build/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+const myName = 'Mateus';
+let myLastName;
+let myAge; // it can hold a string or a number
+let isLoading;
+let album; // any type of data, string, number, boolean. (avoid this)
+const sum = (a, b) => {
+    return a + b;
+};
---
+++ TypeScript/02 - Basic Types/src/main.ts
@@ -0,0 +1,9 @@
+const myName: string = 'Mateus'
+let myLastName: string
+let myAge: number | string // it can hold a string or a number
+let isLoading: boolean
+let album: any // any type of data, string, number, boolean. (avoid this)
+
+const sum = (a: number, b:number): number => {
+  return a + b
+}
